6fc60a6f3e81b637ff3c7f31aa0ea826108a67bb
George Flores <georgeflores886@gmail.com>
2020-06-29 16:48:07 -0400

    modified table.js and created header.js

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,13 +3,14 @@ import React from 'react'
 function Table() {
     return (
         <div>
-            <table class="table">
+            <table className="table">
                 <thead>
                     <tr>
-                        <th scope="col">#</th>
+                        <th scope="col">ID</th>
                         <th scope="col">First</th>
                         <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
+                        <th scope="col">Phone</th>
+                        <th scope="col">Email</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -17,19 +18,24 @@ function Table() {
                         <th scope="row">1</th>
                         <td>Mark</td>
                         <td>Otto</td>
-                        <td>@mdo</td>
+                        <td>704-555-555</td>
+                        <td>ottoman@example.com</td>
                     </tr>
                     <tr>
                         <th scope="row">2</th>
                         <td>Jacob</td>
                         <td>Thornton</td>
+                        <td>704-555-555</td>
                         <td>@fat</td>
+                        
                     </tr>
                     <tr>
                         <th scope="row">3</th>
                         <td>Larry</td>
                         <td>the Bird</td>
+                        <td>704-555-555</td>
                         <td>@twitter</td>
+                     
                     </tr>
                 </tbody>
             </table>
