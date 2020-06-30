f827305a6f3e80657734570dd8b4dd1321f95b42
George Flores <georgeflores886@gmail.com>
2020-06-30 16:04:04 -0400

    added tablebody component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import TableBody from './TableBody'
 function Table() {
     return (
-        <div>
+        
             <table className="table">
                 <thead>
                     <tr>
@@ -13,35 +13,8 @@ function Table() {
                         <th scope="col">Email</th>
                     </tr>
                 </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>704-555-555</td>
-                        <td>ottoman@example.com</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>704-555-555</td>
-                        <td>thornton@example.com</td>
-                        
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>704-555-555</td>
-                        <td>birdman@example.com</td>
-                     
-                    </tr>
-                </tbody>
+               <TableBody/>
             </table>
-
-        </div>
-
     )
 
 }
---
+++ src/components/TableBody.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const TableBody = () => {
+    return (
+        <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>704-555-555</td>
+                        <td>ottoman@example.com</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>704-555-555</td>
+                        <td>thornton@example.com</td>
+                        
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Larry</td>
+                        <td>the Bird</td>
+                        <td>704-555-555</td>
+                        <td>birdman@example.com</td>
+                     
+                    </tr>
+                </tbody>
+    )
+}
+export default TableBody;
