3b744a89d430fdb73ff0ef9d60192faa903e8794
George Flores <georgeflores886@gmail.com>
2020-06-30 15:57:04 -0400

    added wrapper component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react';
 import './App.css';
 import Table from './components/Table'
 import Header from './components/Header'
+import Wrapper from './components/Wrapper'
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Table/>
+    <Wrapper>
 
+    <Header/>
+
+      <Table/>
+      
+    </Wrapper>
     </div>
   );
 }
