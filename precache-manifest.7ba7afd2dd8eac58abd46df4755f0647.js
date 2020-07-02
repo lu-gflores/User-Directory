ac7bc043154c6e7d9c2146af0a082862e2653e16
George Flores <georgeflores886@gmail.com>
2020-07-02 14:49:06 -0400

    Updates

---
+++ precache-manifest.7ba7afd2dd8eac58abd46df4755f0647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad7f181af88d8f06814c7c367810ecc",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "cfaea578a1a16a672723",
+    "url": "/Employee-Directory/static/css/main.83541ba3.chunk.css"
+  },
+  {
+    "revision": "1aefc3cbe3d856204761",
+    "url": "/Employee-Directory/static/js/2.ba1dd176.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.ba1dd176.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfaea578a1a16a672723",
+    "url": "/Employee-Directory/static/js/main.31981d73.chunk.js"
+  },
+  {
+    "revision": "d835bd89e90a2d42677d",
+    "url": "/Employee-Directory/static/js/runtime-main.85115c37.js"
+  }
+]);
